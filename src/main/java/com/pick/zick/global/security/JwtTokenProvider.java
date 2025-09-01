cf9b6c221e350aa77e20930ccd0d77294ca40ef8
dayeon <dana090825@gmail.com>
2025-09-01 12:26:20 +0900

    feat : JWT 생성, 검증 기능 구현

--- src/main/java/com/pick/zick/global/security/JwtTokenProvider.java
+++ src/main/java/com/pick/zick/global/security/JwtTokenProvider.java
@@ -1,4 +1,38 @@
 package com.pick.zick.global.security;
 
+import com.pick.zick.global.security.userdetails.SecurityUserDetails;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtTokenProvider {
+
+    @Value("${app.jwt.secret:비밀키 예시}")
+    private String secretKey;
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            return false;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String getUserId(String token) {
+        Claims claims = (Claims) Jwts.parser()
+                .setSigningKey(secretKey)
+                .parse(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public Authentication buildAuthentication(SecurityUserDetails principal) {
+        return new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+    }
 }
