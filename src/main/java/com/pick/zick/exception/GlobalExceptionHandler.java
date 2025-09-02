8ae6091d026763b8895b0ee6e3199013b0697a80
dayeon <dana090825@gmail.com>
2025-09-02 14:49:39 +0900

    rename : Combining errors

--- src/main/java/com/pick/zick/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pick/zick/exception/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.pick.zick.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Map<String, Object>> handleBusinessException(BusinessException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("type", ex.getErrorCode().name());
+        body.put("statusCode", ex.getErrorCode().getStatus());
+        body.put("message", ex.getErrorCode().getMessage());
+        return ResponseEntity.status(ex.getErrorCode().getStatus()).body(body);
+    }
 }
--- src/main/java/com/pick/zick/exception/UserIdAlreadyExistsException.java
+++
@@ -1,6 +0,0 @@
-package com.pick.zick.exception;
-
-public class UserIdAlreadyExistsException extends RuntimeException {
-    public static final UserIdAlreadyExistsException EXCEPTION = new UserIdAlreadyExistsException();
-    private UserIdAlreadyExistsException(){ super("이미 가입된 아이디입니다"); }
-}
