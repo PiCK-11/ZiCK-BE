5c6a8dff13b38792127b388a358ab4e2ef078dd6
root <1210thatman@gmail.com>
2025-09-29 18:12:33 +0900

    feat: UserFacade 생성

--- src/main/java/com/pick/zick/domain/user/facade/UserFacade.java
+++ src/main/java/com/pick/zick/domain/user/facade/UserFacade.java
@@ -1,4 +1,22 @@
 package com.pick.zick.domain.user.facade;
 
+import com.pick.zick.domain.user.entity.User;
+import com.pick.zick.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserFacade {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return
+    }
+
+    public User getUserById(String userId) {
+        return userRepository.findById(userId).orElse(() -> );
+    }
 }
