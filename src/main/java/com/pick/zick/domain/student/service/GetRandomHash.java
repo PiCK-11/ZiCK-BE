64ade77f867f27c8f357ad521b97d96771a61271
root <1210thatman@gmail.com>
2025-10-01 01:34:24 +0900

    feat: 랜덤 해시값 생성 및 저장 기능 개발

--- src/main/java/com/pick/zick/domain/student/service/GetRandomHash.java
+++ src/main/java/com/pick/zick/domain/student/service/GetRandomHash.java
@@ -6,17 +6,39 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 @Component
 @RequiredArgsConstructor
 public class GetRandomHash {
     private final RedisTemplate<String, String> redisTemplate;
     private final UserFacade userFacade;
 
-    public String makeHash() {
+    public String makeHash() throws NoSuchAlgorithmException {
         User user = userFacade.getCurrentUser();
         Long studentId = user.getId();
         System.out.println(studentId);
 
-        return "최선재";
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        String rawValue = studentId.toString() + timestamp;
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(rawValue.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) hex.append(String.format("%02x", b));
+
+        saveHashToRedis(hex.toString(),  studentId);
+        return hex.toString();
+    }
+
+    private void saveHashToRedis(String hash, Long studentId) {
+        //나중에 300L(5분) -> 30L(30초)로 변경할 것
+        redisTemplate.opsForValue().set(hash, studentId.toString(), 300L, TimeUnit.SECONDS);
     }
 }
