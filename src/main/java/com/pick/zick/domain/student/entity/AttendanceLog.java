8ce0af5d14a25884ede55cf373a05f42e2598e3e
root <1210thatman@gmail.com>
2025-10-01 23:05:30 +0900

    feat: Repository 생성

--- src/main/java/com/pick/zick/domain/student/entity/AttendanceLog.java
+++ src/main/java/com/pick/zick/domain/student/entity/AttendanceLog.java
@@ -1,4 +1,34 @@
 package com.pick.zick.domain.student.entity;
 
+import com.pick.zick.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@DynamicUpdate
 public class AttendanceLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MealType mealType;
+
+    @Column(nullable = false)
+    private Boolean status; //출입 허가 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    public AttendanceLog(User user, MealType mealType, Boolean status) {
+        this.user = user;
+        this.mealType = mealType;
+        this.status = status;
+    }
 }
