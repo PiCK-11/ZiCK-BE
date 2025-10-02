9bedd7e637076f84711e2a8a3f61a937ea5d0836
root <1210thatman@gmail.com>
2025-10-02 16:16:34 +0900

    feat: controller 작성

--- src/main/java/com/pick/zick/domain/student/controller/StudentController.java
+++ src/main/java/com/pick/zick/domain/student/controller/StudentController.java
@@ -2,11 +2,14 @@ package com.pick.zick.domain.student.controller;
 
 import com.pick.zick.domain.student.dto.request.CheckCanEnterRequest;
 import com.pick.zick.domain.student.dto.response.CheckCanEnterResponse;
+import com.pick.zick.domain.student.dto.response.ExcelResponse;
+import com.pick.zick.domain.student.service.AttendanceExcelService;
 import com.pick.zick.domain.student.service.CheckCanEnterService;
 import com.pick.zick.domain.student.service.GetRandomHash;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 @RestController
@@ -14,6 +17,7 @@ import java.security.NoSuchAlgorithmException;
 public class StudentController {
     private final GetRandomHash getRandomHash;
     private final CheckCanEnterService checkCanEnterService;
+    private final AttendanceExcelService attendanceExcelService;
 
     @GetMapping("/qr")
     public String getRandomHash() throws NoSuchAlgorithmException {
@@ -23,5 +27,10 @@ public class StudentController {
     @PostMapping("/attendances")
     public CheckCanEnterResponse checkCanEnterService(@RequestBody CheckCanEnterRequest checkCanEnterRequest) {
         return checkCanEnterService.execute(checkCanEnterRequest.getKey());
+    }
+
+    @GetMapping("/attendance/excel")
+    public ExcelResponse getAttendanceExcel() throws IOException {
+        return attendanceExcelService.generateExcel();
     }
 }
